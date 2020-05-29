d0cefdc575f3b2e8f11a195abb90f9ef636493bd
abdul2020 <git.generalassemb.ly>
2020-05-29 18:31:02 +0300

    set up dev environment + add scss + fix cors&bodyParser

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,15 @@
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 
-console.log(checkForName);
+// console.log(checkForName);
+
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/form.scss';
+import './styles/footer.scss';
+import './styles/header.scss';
 
 // alert("I EXIST")
-console.log('CHANGE!!');
-console.log('CHANGE!!');
 console.log('CHANGE!!');
 
 export { checkForName, handleSubmit };
